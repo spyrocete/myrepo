2d370a01aef64b4dd8187acbdc8f20219af46c3e
spyrocete <32818474+spyrocete@users.noreply.github.com>
2019-11-18 19:19:45 -0700

    added

--- other/arch/apps_install.bash
+++ other/arch/apps_install.bash
@@ -17,13 +17,21 @@ xsel
 xdotool
 cdspell
 shopt
+terminator
+emacs
+libreoffice-fresh
+anamnesis
+mplayer
+git
+lutris
 )
 # xsel will output highlighted text to terminal
 # xdotool can automate gui things
 # cdspell will make is so if you misspell a dirrectory it will just guess
 # shopt can turn programs on and off
 # cmatrix 
-
+# anamnesis clipboard manager
+# lutris: installing games
 
 pacman --noconfirm -Syu
 #pacman --noconfirm -S $(< laptop_pkglist.txt)
